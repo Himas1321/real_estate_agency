515270de3fe4a8cdc45698fcccbd7bd456b03ead
Himas1321 <nikita5045@mail.ru>
2026-02-27 00:01:17 +0900

    Normalize Flat.phonenumbers into Flat.owner_pure_phone.

--- property/models.py
+++ property/models.py
@@ -7,8 +7,11 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
+    owners_phonenumber = models.CharField(
+        'Номер владельца', max_length=20,)
+
     owner_pure_phone = PhoneNumberField(
-        'Стандартный телефон',
+        'Нормализованный телефон владельца',
         blank=True,
         null=True,
         region="RU",
