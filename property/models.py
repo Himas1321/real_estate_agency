400a1f2768e51e8e3cd971a43976f563996ade86
Himas1321 <nikita5045@mail.ru>
2026-02-27 21:09:56 +0900

    migrate flat-owner relation to owner model

--- property/models.py
+++ property/models.py
@@ -6,17 +6,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField(
-        'Номер владельца', max_length=20,)
-
-    owner_pure_phone = PhoneNumberField(
-        'Нормализованный телефон владельца',
-        blank=True,
-        null=True,
-        region="RU",
-    )
-
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
